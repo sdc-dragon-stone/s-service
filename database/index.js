1b0c1974aa4163d3055dc8ea89e8044aef7a096b
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-03-27 22:20:29 -0700

    Separate seeding function into separate file

---
+++ database/index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const faker = require('faker');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+mongoose.connect('mongodb://localhost/morehomes', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
+
+const homeSchema = new mongoose.Schema({
+  _id: Number,
+  pictureUrl: String,
+  typeOfHome: String,
+  city: String,
+  description: String,
+  price: Number,
+  rating: String,
+  reviews: Number
+}, { _id: false });
+
+homeSchema.plugin(AutoIncrement);
+
+const Home = mongoose.model('Home', homeSchema);
+
+// the array of sample homes: sampleData.homes
+function saveHome(homeUrl) {
+  const newHome = new Home({
+    pictureUrl: homeUrl,
+    typeOfHome: `entire ${faker.lorem.word()}`,
+    city: faker.address.city(),
+    description: faker.random.words(3),
+    price: faker.random.number({ min: 35, max: 150 }),
+    rating: `Stars: ${faker.random.number({ min: 3.5, max: 5 })}`,
+    reviews: faker.random.number({ min: 20, max: 50 })
+  });
+
+  newHome.save();
+}
+
+module.exports = {
+  saveHome
+};
