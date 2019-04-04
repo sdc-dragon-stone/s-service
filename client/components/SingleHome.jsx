5ad3baa46ce0a5e4f2fd0b48c825a976b5a62684
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-04-03 22:05:12 -0700

    Basic implementation of a picture carousel

--- client/components/SingleHome.jsx
+++ client/components/SingleHome.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 function SingleHome(props) {
   return (
     <div className="homePic">
-      <img src={props.pictureUrl} key={props.id} alt="Place for rent" width="350" height="225" />
-      <div> {props.typeOfHome} &#183; {props.city}</div>
-      <div>{props.description}</div>
-      <div>${props.price} per night</div>
-      <div> {props.rating} | {props.reviews} reviews</div>
+      <a href="https://thisrentaldoesnotexist.com" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+        <img src={props.pictureUrl} key={props.id} alt="Place for rent" width="315" height="210" />
+        <div> {props.typeOfHome} &#183; {props.city}</div>
+        <div>{props.description}</div>
+        <div>${props.price} per night</div>
+        <div> {props.rating} | {props.reviews} reviews</div>
+      </a>
     </div>
   );
 }
